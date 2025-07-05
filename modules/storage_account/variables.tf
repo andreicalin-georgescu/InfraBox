6a9bafd9f0f44d09b51098997cf3d6f2ee4d67d2
Andrei-Calin Georgescu <calinandrei8@gmail.com>
2025-07-05 14:08:38 +0300

    Refactor Terraform modules for consistent formatting

--- modules/storage_account/variables.tf
+++ modules/storage_account/variables.tf
@@ -1,16 +1,16 @@
 variable "name" {
-    description = "Name of the storage account"
-    type        = string
+  description = "Name of the storage account"
+  type        = string
 }
 
 variable "resource_group_name" {
-    description = "Resource group in which to create the storage account"
-    type        = string
+  description = "Resource group in which to create the storage account"
+  type        = string
 }
 
 variable "location" {
-    description = "Azure region for the storage account"
-    type        = string
+  description = "Azure region for the storage account"
+  type        = string
 }
 
 variable "account_tier" {
@@ -26,7 +26,7 @@ variable "account_replication_type" {
 }
 
 variable "tags" {
-    description = "Tags to apply to the storage account"
-    type        = map(string)
-    default     = {}
+  description = "Tags to apply to the storage account"
+  type        = map(string)
+  default     = {}
 }
